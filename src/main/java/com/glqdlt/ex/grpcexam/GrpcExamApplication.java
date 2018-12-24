ccabab6eda19298c299ee05d4ec226bda3c8c667
glqdlt <dlfdnd0725@gmail.com>
2018-12-24 09:18:31 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/grpcexam/GrpcExamApplication.java
@@ -0,0 +1,14 @@
+package com.glqdlt.ex.grpcexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcExamApplication.class, args);
+	}
+
+}
+
